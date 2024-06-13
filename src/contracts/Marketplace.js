efcf66a516c23a0eee5a9100b354b23eff6e628a
工藤权一 <115307551@qq.com>
2024-06-14 02:11:16 +0800

    contracts

--- src/contracts/Marketplace.js
+++ src/contracts/Marketplace.js
@@ -4,7 +4,8 @@ import contractABI from './C2CPlatform.json'; // 导入智能合约 ABI JSON 文
 
 const web3 = new Web3(Web3.givenProvider);
 const abi = contractABI.abi; // 从 JSON 文件中获取 ABI
-const contractAddress = '0xYourContractAddress'; // 智能合约的地址
+const contractAddress = '0x3bfccbcab52b0e78e1100ebfa2b357accc5ee53c'; // 智能合约的地址
+//https://sepolia.etherscan.io/tx/0x61c3ac67ffa59fe82d7e5547ac2cb3797730c742ecdccd88df447f1752970324
 const contract = new web3.eth.Contract(abi, contractAddress);
 
 export default contract;
